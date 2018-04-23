1f12defaa4b883cc5bc0fec4a783ba59ca816e7e
Vibha Alangar <alangavr@rose-hulman.edu>
2018-04-23 11:30:12 -0400

    implemented anonymous auth (lols this was easy)

--- Password Keeper/Password Keeper/AppDelegate.swift
+++ Password Keeper/Password Keeper/AppDelegate.swift
@@ -23,6 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
     
     FirebaseApp.configure()
+    
     let showLoginScreen = false
     if showLoginScreen {
       showLoginViewController();
